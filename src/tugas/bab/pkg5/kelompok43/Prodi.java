8e796b928df27b582af57c1ebbc1c885dbd70cf2
clamerry <38363375+clamerry@users.noreply.github.com>
2018-04-30 12:32:51 +0700

    Add files via upload

---
+++ src/tugas/bab/pkg5/kelompok43/Prodi.java
@@ -0,0 +1,14 @@
+package tugasbab5kelompok43;
+public class Prodi {
+    String prodi1, prodi2, prodi3;
+    Prodi (String k,  String p, String l){
+    prodi1 = k;
+    prodi2 = p;
+    prodi3 = l;
+    }
+    String getProdi(){
+        return "\n 1. " + prodi1 + "\n 2. " + prodi2 + "\n 3. " + prodi3;
+    }
+}
+    
+    
