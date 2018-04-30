552d2e2196cdad9d4265881c0f5c77c000da908a
clamerry <38363375+clamerry@users.noreply.github.com>
2018-04-30 12:38:35 +0700

    Update TugasBab5Kelompok43.java

--- src/tugas/bab/pkg5/kelompok43/TugasBab5Kelompok43.java
+++ src/tugas/bab/pkg5/kelompok43/TugasBab5Kelompok43.java
@@ -1,22 +1,26 @@
 package tugas.bab.pkg5.kelompok43;
-public class TugasBab5Kelompok43 {
-
+class TugasBab5Kelompok43 extends Prodi {
+    String prodi3;
+    TugasBab5Kelompok43 (String k, String p, String l){
+        super(k, p, l);
+            prodi3 = l;
+    }
 
     public static void perkenalan() {
         fungsi objek = new fungsi();
         objek.pembatasa();
         System.out.println("            Selamat datang di Program Kelompok 43");
-    }    
+    }
+    
+    String getProdi (String k, String p, String l){
+        return super.getProdi() + Ün3. "+ prodi3;
+    }
 
     public static void main(String[] args) {
         perkenalan();
         InputOutput java = new InputOutput();            
-            System.out.println ("1. Teknik Komputer");
-            System.out.println ("1. Teknik Perkapalan");
-            System.out.println ("3. Teknik Lingkungan");
-                
+        TugasBab5Kelompok43 a1 = new TugasBab5Kelompok43("Teknik Komputer", "Teknik Perkapalan, "Teknik Lingkungan");
+        System.out.println(a1.getProdi());
         java.jurusan();
-    
-   
     }
 }
